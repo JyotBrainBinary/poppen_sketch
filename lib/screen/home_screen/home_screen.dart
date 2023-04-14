eab763abfdd4bdd885b7a0a7189cc94d8d52a853
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2023-04-14 10:52:31 +0530

    change j

--- lib/screen/home_screen/home_screen.dart
+++ lib/screen/home_screen/home_screen.dart
@@ -16,18 +16,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
   List videoUrl = [
     'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-    'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+    'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+    'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'
   ];
 
-  /*PageController pageController = PageController(
-    initialPage: 0
-  );
-
-  @override
-  void dispose() {
-    pageController.dispose();
-    super.dispose();
-  }*/
 
   @override
   Widget build(BuildContext context) {
